809673103a3f9881f204a6cab3fc1b95e3ad9ac1
Marcin Parafiniuk <mp347235@students.mimuw.edu.pl>
2017-03-04 15:45:13 +0100

    dużo rzeczy

--- src/regex/lexingengine.cpp
+++ src/regex/lexingengine.cpp
@@ -1,20 +1,10 @@
 #include <iostream>  //debugging
+
 #include <vector>
+#include <fstream>
 
 #include "lexingengine.hpp"
 #include "regex.h"
-
-const std::string regexStrs[] = {
-    "([0-9])+",
-    "([a-z])+",
-    "for",
-};
-
-const std::string tokenNames[] = {
-    "NUMBER",
-    "ID",
-    "FOR",
-};
 
 const std::string printStepResult(StepResult result) {
     switch(result) {
@@ -27,28 +17,88 @@ const std::string printStepResult(StepResult result) {
     }
 }
 
-LexingEngine::LexingEngine() {
-    for (int i = 0; i < (sizeof(regexStrs)/sizeof(regexStrs[0])); i++) {
-        RegEx* re = new RegEx;
-        re->Compile(regexStrs[i]);
-        regexps.push_back({re, tokenNames[i]});
+///////////////// MAYBE TOKEN
+
+MaybeToken::MaybeToken(const std::string& name_, const std::string& regex_)
+    : name(name_), regexSrc(regex_)
+{
+    regex = new RegEx;
+    reset();
+}
+
+MaybeToken::~MaybeToken() {
+    delete regex;
+}
+
+StepResult MaybeToken::step(char c) {
+    return regex->Step(c);
+}
+
+StepResult MaybeToken::stepBack() {
+    return regex->StepBack();
+}
+
+void MaybeToken::reset() {
+    regex->Compile(regexSrc);
+}
+
+///////////////// MAYBE PUNCTUATOR
+
+MaybePunctuator::MaybePunctuator(const std::string& p)
+    : punctuator(p)
+{}
+
+StepResult MaybePunctuator::step(char c){
+    if (counter > punctuator.size()){
+        counter++;
+        return REJECTED;
     }
+    if(punctuator[counter] != c) {
+        counter++;
+        return REJECTED;
+    }
+    counter++;
+    if(counter == punctuator.size()) {
+        return ACCEPTED;
+    }
+    return UNDECIDED;
+}
+
+StepResult MaybePunctuator::stepBack(){
+}
+
+void MaybePunctuator::reset(){
+    counter = 0;
+}
+
+///////////////// LEXING ENGINE
+
+LexingEngine::LexingEngine() {
+    std::ifstream keywordsFile("../data/keywords.txt");
+    std::string keyword;
+    while(keywordsFile >> keyword) {
+        tokens.push_back(new MaybeToken(keyword, keyword));
+    }
+    std::ifstream punctuatorsFile("../data/punctuators.txt");
+    std::string punctuator;
+    while(punctuatorsFile >> punctuator) {
+        punctuators.push_back(new MaybePunctuator(punctuator));
+    }
+    tokens.push_back(new MaybeToken("IDENTIFIER", "([a-z])+"));
+    tokens.push_back(new MaybeToken("NUMBER", "([0-9])+"));
 }
 
 LexingEngine::~LexingEngine() {
-    for (int i = 0; i < (sizeof(regexStrs)/sizeof(regexStrs[0])); i++) {
-        delete regexps[i].first;
-    }
 }
 
 void LexingEngine::processKeypress(char32_t c) {
-    for (int i = 0; i < regexps.size(); i++) {
+    for (MaybePunctuator* mp : punctuators) {
         StepResult result;
         if (c == 8) {
-            result = regexps[i].first->StepBack();
+            result = mp->stepBack();
         } else {
-            result = regexps[i].first->Step(c);
+            result = mp->step(c);
         }
-        printf("%s\t%s\r\n", regexps[i].second.c_str(), printStepResult(result).c_str());
+        printf("%s\t%s\r\n", mp->punctuator.c_str(), printStepResult(result).c_str());
     }
 }
--- src/regex/lexingengine.hpp
+++ src/regex/lexingengine.hpp
@@ -4,11 +4,48 @@
 
 #include "regex.h"
 
+class MaybeRegex {
+public:
+    virtual StepResult step(char c) = 0;
+    virtual StepResult stepBack() = 0;
+    virtual void reset() = 0;
+};
+
+class MaybeToken
+    : public MaybeRegex
+{
+public:
+    MaybeToken(const std::string&, const std::string&);
+    ~MaybeToken();
+    StepResult step(char c);
+    StepResult stepBack();
+    void reset();
+
+    const std::string name;
+    const std::string regexSrc;
+    RegEx* regex;
+};
+
+class MaybePunctuator
+    : public MaybeRegex
+{
+public:
+    MaybePunctuator(const std::string&);
+    StepResult step(char c);
+    StepResult stepBack();
+    void reset();
+
+    const std::string punctuator;
+private:
+    int counter = 0;
+};
+
 class LexingEngine {
 public:
     LexingEngine();
     ~LexingEngine();
     void processKeypress(char32_t c);
 private:
-    std::vector<std::pair<RegEx*, std::string> > regexps;
+    std::vector<MaybeToken*> tokens;
+    std::vector<MaybePunctuator*> punctuators;
 };
